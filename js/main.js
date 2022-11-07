8309899c60c9c2f13c6e1b3a26cc24870399b45c
Therakar <matteo.pau200@gmail.com>
2022-11-07 16:38:11 +0100

    Milestone 0 : crea un array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 "use strict"
+
+//array che conterrà gli oggetti
+ const people = [
+    //oggetto 
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    }
+    //oggetto
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    }
+    //oggetto
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    }
+    //oggetto
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    }
+    //oggetto
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    }
+    //oggetto
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    }
+ ]
