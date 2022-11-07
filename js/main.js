8476274d0a6141ffe9fcee85162a68a5852404a9
Therakar <matteo.pau200@gmail.com>
2022-11-07 16:51:37 +0100

    Milestone 1: stampa l'array con tutte le info nella console

--- js/main.js
+++ js/main.js
@@ -7,35 +7,44 @@
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         image: 'wayne-barnett-founder-ceo.jpg',
-    }
+    },
     //oggetto
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         image: 'angela-caroll-chief-editor.jpg',
-    }
+    },
     //oggetto
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
         image: 'walter-gordon-office-manager.jpg',
-    }
+    },
     //oggetto
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         image: 'angela-lopez-social-media-manager.jpg',
-    }
+    },
     //oggetto
     {
         name: 'Scott Estrada',
         role: 'Developer',
         image: 'scott-estrada-developer.jpg',
-    }
+    },
     //oggetto
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
         image: 'barbara-ramos-graphic-designer.jpg',
     }
- ]
+ ];
+
+
+ //ciclo for che cicla sull'array di oggetti 
+ for( let i = 0; i < people.length; i++){
+    const employees = people[i];
+    console.log (employees.name, employees.role, employees.image);
+    
+ }
+ 
